a06d691532b7eb0f83f14831608becf1b0c3a00f
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-05-27 19:06:35 +0200

    updated category and program

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -11,21 +11,19 @@ create table item (
   foreign key(user_id) references user(id)
 );
 
+
 create table category (
-  id int unsigned auto_increment not null,
- name varchar(255) not null,
-  category_id int unsigned not null,
-  foreign key(category_id) references category(id)
+  id int unsigned primary key auto_increment not null,
+  email varchar(255) not null unique,
+  password varchar(255) not null
 );
 
 
 create table program (
-  id int unsigned auto_increment not null,
+  id int unsigned primary key auto_increment not null,
   title varchar(255) not null,
   synopsis text not null,
   poster text not null,
   country text not null,
   year int not null,
-  program_id int unsigned not null,
-  foreign key(program_id) references program(id)
 );
